baac710945a2341d78237af9cae5b121062db6d1
noobandy <noobandy1364@gmail.com>
2016-01-02 12:26:43 +0530

    using usedAt instead of used for logging the timestamp when an otp was used

--- models/Otp.js
+++ models/Otp.js
@@ -1,44 +1,50 @@
 var mongoose = require("mongoose"),
-	OtpSchema,
-	Otp;
+    OtpSchema,
+    Otp;
 
 OtpSchema = mongoose.Schema({
-	project : {
-		type : mongoose.Schema.ObjectId,
-		required : true,
-		ref : "Project"
-	},
-	mobileNumber : {
-		type : String,
-		required : true,
-		match : /[0-9]{10}/
-	},
-	key : {
-		type : String,
-		required : true
-	},
-	//unix timestamp
-	validTill : {
-		type : Number,
-		required : true
-	},
-	attemptsLeft : {
-		type : Number,
-		required : true
-	},
-	used : {
-		type : Boolean,
-		default : false
-	}
+    project: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+        ref: "Project"
+    },
+    mobileNumber: {
+        type: String,
+        required: true,
+        match: /[0-9]{10}/
+    },
+    key: {
+        type: String,
+        required: true
+    },
+    //unix timestamp
+    validTill: {
+        type: Number,
+        required: true
+    },
+    attemptsLeft: {
+        type: Number,
+        required: true
+    },
+    //unix timestamp
+    usedAt: {
+        type: Number
+    }
 });
 
 
-OtpSchema.statics.findByRequestId = function(requestId, cb) {
-	Otp.findById(requestId, function(err, otp) {
-		if(err) return cb(err);
+OtpSchema.statics.findByRequestId = function (requestId, cb) {
+    Otp.findById(requestId, function (err, otp) {
+        if (err) return cb(err);
 
-		return cb(null, otp);
-	});
+        return cb(null, otp);
+    });
+};
+
+OtpSchema.methods.isValid = function () {
+    var now = new Date().getTime();
+
+    return !this.usedAt && (this.validTill > now);
 };
 
 
--- models/Project.js
+++ models/Project.js
@@ -1,124 +1,120 @@
 var mongoose = require("mongoose"),
-	ProjectSchema,
-	Project,
-	path = require("path"),
-	Otp = require(path.join(__dirname, "Otp")),
-	otpGenerator = require(path.join(__dirname, "../models/OtpGenerator")),
-	SMSHelper = require(path.join(__dirname, "../models/SMSHelper"));
+    ProjectSchema,
+    Project,
+    path = require("path"),
+    Otp = require(path.join(__dirname, "Otp")),
+    otpGenerator = require(path.join(__dirname, "../models/OtpGenerator")),
+    SMSHelper = require(path.join(__dirname, "../models/SMSHelper"));
 
 ProjectSchema = mongoose.Schema({
-	name : {
-		type : String,
-		required : true
-	},
-	apiKey : {
-		//secret slug(_id)
-		type : String,
-		required : true
-	},
-	config : {
-		//time to live (validity period for otp in milliseconds)
-		otpValidityPeriod : {
-			type : Number,
-			//default is 3 min (180000 milliseconds)
-			default : 180000
-		},
-		//how many attempts are allowed for otp
-		otpMaxAllowedAttempts : {
-			type : Number,
-			default : 3
-		},
-		//otp length
-		otpLength : {
-			type : Number,
-			default: 6
-		}
-	},
-	user : {
-		type : mongoose.Schema.ObjectId,
-		required : true,
-		ref : "User"
-	},
-	deletedAt : {
-		type : Date
-	}
+    name: {
+        type: String,
+        required: true
+    },
+    apiKey: {
+        //secret slug(_id)
+        type: String,
+        required: true
+    },
+    config: {
+        //time to live (validity period for otp in milliseconds)
+        otpValidityPeriod: {
+            type: Number,
+            //default is 3 min (180000 milliseconds)
+            default: 180000
+        },
+        //how many attempts are allowed for otp
+        otpMaxAllowedAttempts: {
+            type: Number,
+            default: 3
+        },
+        //otp length
+        otpLength: {
+            type: Number,
+            default: 6
+        }
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        required: true,
+        ref: "User"
+    },
+    deletedAt: {
+        type: Date
+    }
 });
 
-ProjectSchema.statics.findAllProjectsOfUser = function(user, cb) {
-	Project.find({user : user._id, deletedAt : null}, function(err, projects) {
-		if(err) return cb(err);
+ProjectSchema.statics.findAllProjectsOfUser = function (user, cb) {
+    Project.find({user: user._id, deletedAt: null}, function (err, projects) {
+        if (err) return cb(err);
 
-		return cb(null, projects);
-	});
+        return cb(null, projects);
+    });
 };
 
-ProjectSchema.statics.findByApiKey = function(apiKey, cb) {
-	Project.findOne({apiKey : apiKey, deletedAt : null}, function(err, project) {
-		if(err) return cb(err);
+ProjectSchema.statics.findByApiKey = function (apiKey, cb) {
+    Project.findOne({apiKey: apiKey, deletedAt: null}, function (err, project) {
+        if (err) return cb(err);
 
-		return cb(null, project);
-	});
+        return cb(null, project);
+    });
 };
 
-ProjectSchema.methods.delete = function(cb) {
-	this.deletedAt = new Date();
-	this.save(function(err,doc) {
-		if(err) return cb(err);
-		return cb(null, doc);
-	});
+ProjectSchema.methods.delete = function (cb) {
+    this.deletedAt = new Date();
+    this.save(function (err, doc) {
+        if (err) return cb(err);
+        return cb(null, doc);
+    });
 };
 
-ProjectSchema.methods.requestOTP = function(mobileNumber, cb) {
-	var otp = new Otp({mobileNumber : mobileNumber});
+ProjectSchema.methods.requestOTP = function (mobileNumber, cb) {
+    var otp = new Otp({mobileNumber: mobileNumber});
 
-	otp.project = this._id;
+    otp.project = this._id;
 
-	otp.key = otpGenerator(this.config.otpLength);
+    otp.key = otpGenerator(this.config.otpLength);
 
-	otp.validTill = new Date().getTime() + this.config.otpValidityPeriod;
+    otp.validTill = new Date().getTime() + this.config.otpValidityPeriod;
 
-	otp.attemptsLeft = this.config.otpMaxAllowedAttempts;
+    otp.attemptsLeft = this.config.otpMaxAllowedAttempts;
 
-	otp.save(function(err, otp) {
-		if(err) return cb(err);
+    otp.save(function (err, otp) {
+        if (err) return cb(err);
 
-		SMSHelper.sendOTP(otp.mobileNumber, otp.key);
+        SMSHelper.sendOTP(otp.mobileNumber, otp.key);
 
-		return cb(null, otp._id);
-	});
+        return cb(null, otp._id);
+    });
 };
 
-ProjectSchema.methods.verifyOTP = function(requestId, userResponse, cb) {
-	Otp.findByRequestId(requestId, function(err, otp) {
-		var now = new Date().getTime();
+ProjectSchema.methods.verifyOTP = function (requestId, userResponse, cb) {
+    Otp.findByRequestId(requestId, function (err, otp) {
+        var verified = false;
 
-		var verified = false;
+        if (err) return cb(err);
 
-		if(err) return cb(err);
-
-		if(otp === null) return cb({name : "otp not found", message: "otp not found"});
+        if (otp === null) return cb({name: "otp not found", message: "otp not found"});
 
 
-		if(!otp.used && otp.validTill >= now && otp.attemptsLeft > 0) {
+        if (otp.isValid() && otp.attemptsLeft > 0) {
 
-			otp.attemptsLeft = otp.attemptsLeft - 1;
+            otp.attemptsLeft = otp.attemptsLeft - 1;
 
-			if(otp.key === userResponse) {
-				otp.used = true;
-				verified = true;
-			}
-		}
+            if (otp.key === userResponse) {
+                otp.usedAt = new Date().getTime();
+                verified = true;
+            }
+        }
 
-		otp.save(function(err, otp) {
-			if(err) return cb(err);
+        otp.save(function (err, otp) {
+            if (err) return cb(err);
 
-			return cb(null, verified);
-		});
+            return cb(null, verified);
+        });
 
-	});
+    });
 };
-
-
 
 
 Project = mongoose.model("Project", ProjectSchema);
