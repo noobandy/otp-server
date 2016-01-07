b3dabf776134171ace851d738969f05c23039a38
noobandy <noobandy1364@gmail.com>
2016-01-07 17:21:59 +0530

    Contact request schema

--- models/ContactRequest.js
+++ models/ContactRequest.js
@@ -1,50 +1,55 @@
 var mongoose = require("mongoose"),
-	ContactRequestSchema,
-	ContactRequest;
+    ContactRequestSchema,
+    ContactRequest;
 
 
 ContactRequestSchema = mongoose.Schema({
-	firstName : {
-		type : String,
-		required : true,
-		match : /^[a-zA-Z]+ *[a-zA-Z]+$/
-	},
-	lastName : {
-		type : String,
-		required : true,
-		match : /^[a-zA-Z]+ *[a-zA-Z]+$/
-	},
-	emailId : {
-		type : String,
-		required : true
-	},
-	mobileNumber : {
-		type : String,
-		required : true,
-		match : /[0-9]{10}/
-	},
-	message : {
-		type : String,
-		required : true
-	},
-	emailIdVerificationKey : {
-		type : String
-	},
-	mobileNumberVerificationCode : {
-		tyep : Number
-	},
-	emailIdVerified : {
-		type : Boolean,
-		default : false
-	},
-	mobileNumberVerified : {
-		type : Boolean,
-		default : false
-	},
-	repliedAt : {
-		type : Date
-	},
-	remarks : {
-		type : String
-	}
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    emailId: {
+        type: String,
+        required: true
+    },
+    mobileNumber: {
+        type: String,
+        required: true,
+        match: /[0-9]{10}/
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    emailIdVerificationKey: {
+        type: String
+    },
+    mobileNumberVerificationCode: {
+        type: Number
+    },
+    emailIdVerified: {
+        type: Boolean,
+        default: false
+    },
+    mobileNumberVerified: {
+        type: Boolean,
+        default: false
+    },
+    status: {
+        type : String,
+        required : true,
+        enum : ["new", "pending", "completed"],
+        default : "new"
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    remarks: {
+        type: String
+    }
 });
